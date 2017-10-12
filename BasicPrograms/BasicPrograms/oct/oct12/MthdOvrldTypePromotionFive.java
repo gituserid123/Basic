2df5d3dde6300d5f34b6aba69619edc50bc52889
gituser <gituserid@gmail.com>
2017-10-12 18:28:25 +0530

    ignore 1

--- BasicPrograms/BasicPrograms/oct/oct12/MthdOvrldTypePromotionFive.java
+++ BasicPrograms/BasicPrograms/oct/oct12/MthdOvrldTypePromotionFive.java
@@ -7,12 +7,12 @@ class ovrldTpProm {
 	}
 
 	void add(int a, float b, double c) {
-		System.out.println(a - b - c);
+		System.out.println(a - b - c); //fd
 	}
 }
 
 public class MthdOvrldTypePromotionFive {
-
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ovrldTpProm otprm = new ovrldTpProm();
