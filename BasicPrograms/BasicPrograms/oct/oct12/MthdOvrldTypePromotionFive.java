8ab63f127f417604bd1a7432b75c3e0c0e04b864
gituser <gituserid@gmail.com>
2017-10-12 18:25:17 +0530

    commit7

--- BasicPrograms/BasicPrograms/oct/oct12/MthdOvrldTypePromotionFive.java
+++ BasicPrograms/BasicPrograms/oct/oct12/MthdOvrldTypePromotionFive.java
@@ -1,9 +1,23 @@
 package oct12;
 
+class ovrldTpProm {
+	void add(int a, int b) {
+		System.out.println(a + b);
+
+	}
+
+	void add(int a, float b, double c) {
+		System.out.println(a - b - c);
+	}
+}
+
 public class MthdOvrldTypePromotionFive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ovrldTpProm otprm = new ovrldTpProm();
+		otprm.add(1, 4);
+		otprm.add(1, 2.3f, 4);
 
 	}
 
